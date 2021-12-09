24f15118eecf09f16b7529171a287f3d388dc709
elnao <sgrb7yfe@gmail.com>
2021-12-09 09:50:04 -0600

    Update BallPeen.ps1
    Updated to run as an infinate loop instead of being triggered by Scheduled Task every 5 minutes.

--- BallPeen.ps1
+++ BallPeen.ps1
@@ -2,27 +2,37 @@
 #  Alert on Responder Activity
 #  Write Event ID 1337 or 1338 to Application Event Log
 #  https://github.com/elnao/hashes
-#  Date:  05/20/2020
+#  Date:  12/09/2021
 ###################################################
 
-$EventSource = "BallPeen"
-$Event_ID = "1337"
-$EventPriority = "Information"
-$respounder_results = "C:\Program Files\elnao\Respounder.exe" -rhostname | out-String
+while($true)
+{
 
- if ($respounder_results -like "*responder detected*")  {
-   $EventPriority = "Warning"
-   $Event_ID = "1338"
-   }
-else {
-   $EventPriority = "Information"
-   $Event_ID = "1337"
-   }  
+    $EventSource = "BallPeen"
+    $Event_ID = "1337"
+    $EventPriority = "Information"
+    $RandomNumber = get-random -minimum 3 -Maximum 10
+    $RandomNumber = $RandomNumber -replace '\s',''
+    $respounder_results = "C:\Program Files\elnao\Respounder.exe" -hostname "adminsrv-"$RandomNumber| out-String
 
-if ([System.Diagnostics.EventLog]::SourceExists($EventSource) -eq $False) {
-    New-EventLog -LogName Application -Source $EventSource
-    Write-EventLog -LogName Application -EntryType $EventPriority -EventId $Event_ID -Source $EventSource -Message $respounder_results
-  }
-else {
-  Write-EventLog -LogName Application -EntryType $EventPriority -EventId $Event_ID -Source $EventSource -Message $respounder_results
-  }
+
+    if ($respounder_results -like "*responder detected*")  {
+        $EventPriority = "Warning"
+        $Event_ID = "1338"
+     }
+    else {
+        $EventPriority = "Information"
+        $Event_ID = "1337"
+       }   
+
+    if ([System.Diagnostics.EventLog]::SourceExists($EventSource) -eq $False) {
+        New-EventLog -LogName Application -Source $EventSource
+        Write-EventLog -LogName Application -EntryType $EventPriority -EventId $Event_ID -Source $EventSource -Message $respounder_results
+     }
+    else {
+        Write-EventLog -LogName Application -EntryType $EventPriority -EventId $Event_ID -Source $EventSource -Message $respounder_results
+         }
+
+    sleep 300
+
+} 
